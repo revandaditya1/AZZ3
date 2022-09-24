fe685fcab0e3628b48bfd3c73c8fd0197d0b430f
revandaditya1 <113486346+revandaditya1@users.noreply.github.com>
2022-09-24 09:03:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ global.APIKeys = {
 }
 
 // Other
-global.namabot = ['AzzBot-MD']
-global.namaowner = ['YahyaGanzz']
-global.owner = ['6281511480762']
-global.premium = ['6281511480762']
-global.packname = 'AzzBot-MD'
+global.namabot = ['VanzzBot-MD']
+global.namaowner = ['Revand']
+global.owner = ['6289518562932']
+global.premium = ['6289518562932']
+global.packname = 'VanzzBot-MD'
 global.author = 'WhatsApp•Bot'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
